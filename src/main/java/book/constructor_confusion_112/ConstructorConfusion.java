e44a5173e6f6b89714d388fe9a1cfb268df205f8
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2022-02-06 15:08:46 +0300

    Constructor injection confusion, @Value, @PropertySource

---
+++ src/main/java/book/constructor_confusion_112/ConstructorConfusion.java
@@ -0,0 +1,54 @@
+package book.constructor_confusion_112;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+@Service("constructorConfusion")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "book.constructor_confusion_112")
+public class ConstructorConfusion {
+  private String someValue;
+
+  public ConstructorConfusion(String someValue) {
+    System.out.println("ConstructorConfusion(String) called");
+    this.someValue = someValue;
+  }
+
+  // присвоение значения primary-полю бина
+  // @Value use guide: https://www.baeldung.com/spring-value-annotation
+  @Autowired
+  public ConstructorConfusion(
+      @Value("${constructor_confusion_112.value}") int someValue) {
+    System.out.println("ConstructorConfusion(int) called");
+    this.someValue = "Number: " + someValue;
+  }
+  // аналогичный XML выглядит так:
+  /*
+  <bean id="constructorConfusion"
+    class="book.constructor_confusion_112.ConstructorConfusion">
+	  <constructor-arg type="int">
+		  <value>85</value>
+	  </constructor-arg>
+  </bean>
+  */
+
+  @Override
+  public String toString() {
+    return someValue;
+  }
+
+  public static void main(String[] args) {
+    ApplicationContext ctx = new AnnotationConfigApplicationContext(
+        ConstructorConfusion.class);
+    ConstructorConfusion beanConfusion =
+        (ConstructorConfusion) ctx.getBean("constructorConfusion");
+    System.out.println("[BEAN]: " + beanConfusion);
+    ConstructorConfusion manualConfusion = new ConstructorConfusion("programmatically created class");
+    System.out.println("[MANUAL]: " + manualConfusion);
+  }
+}
