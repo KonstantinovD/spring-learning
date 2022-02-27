e771338a5d28a7720a72c80baa596e7c7885fc61
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2022-02-27 12:54:23 +0300

    [JC] wait, notify, locks with conditions, potentially deadlock

---
+++ src/main/java/ch2_plain_java/sn4_threads_wait_notify/WaitNotifyStoreDemo.java
@@ -0,0 +1,102 @@
+package ch2_plain_java.sn4_threads_wait_notify;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class WaitNotifyStoreDemo {
+
+  public static void main(String[] args) {
+    Store store=new Store();
+    Producer producer = new Producer(store);
+    Consumer consumer = new Consumer(store);
+    new Thread(producer).start();
+    new Thread(consumer).start();
+  }
+
+}
+
+// Класс Магазин, хранящий произведенные товары
+class Store{
+  private int product=0;
+  private Lock locker;
+  private Lock locker2;
+  private Condition productState;
+  private Condition productState2;
+
+  public Store(){
+    locker = new ReentrantLock();
+    productState = locker.newCondition();
+
+    locker2 = new ReentrantLock();
+    productState2 = locker.newCondition();
+  }
+
+  public synchronized void get() {
+    while (product < 1) {
+      try {
+        System.out.println(
+            "--- Покупатель не может ничего купить:"
+                + " нет товаров");
+        wait();
+      }
+      catch (InterruptedException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+
+    int operate = operateProduct(-1);
+    System.out.println("Покупатель купил 1 товар." +
+        " Товаров на складе: " + operate);
+    notifyAll();
+  }
+  public synchronized void put() {
+    while (product >= 3) {
+      try {
+        System.out.println(
+            "--- Производитель не может доставить товар:"
+                + " склад полностью заполнен (максимум = 3)");
+        wait();
+      }
+      catch (InterruptedException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+    int operate = operateProduct(1);
+    System.out.println("Производитель добавил 1 товар." +
+        " Товаров на складе: " + operate);
+    notifyAll();
+  }
+
+  private synchronized int operateProduct(int delta) {
+    product += delta;
+    return product;
+  }
+}
+
+// класс Производитель
+class Producer implements Runnable{
+
+  Store store;
+  Producer(Store store){
+    this.store=store;
+  }
+  public void run(){
+    for (int i = 1; i < 6; i++) {
+      new Thread(() -> store.put()).start();
+    }
+  }
+}
+// Класс Потребитель
+class Consumer implements Runnable{
+
+  Store store;
+  Consumer(Store store){
+    this.store=store;
+  }
+  public void run(){
+    for (int i = 1; i < 6; i++) {
+      new Thread(() -> store.get()).start();
+    }
+  }
+}
---
+++ src/main/java/ch2_plain_java/sn5_locks_with_conditions/LockWithConditionsStackDemo.java
@@ -0,0 +1,76 @@
+package ch2_plain_java.sn5_locks_with_conditions;
+
+import java.util.Stack;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockWithConditionsStackDemo {
+
+  Stack<String> stack = new Stack<>();
+  int CAPACITY = 5;
+
+  ReentrantLock lock = new ReentrantLock();
+  Condition stackEmptyCondition = lock.newCondition();
+  Condition stackFullCondition = lock.newCondition();
+
+  public static void main(String[] args) {
+    LockWithConditionsStackDemo lwc = new LockWithConditionsStackDemo();
+    new Thread(() -> {
+      for (int i = 1; i < 6; i++) {
+        int val = i;
+        new Thread(() -> {
+          try {
+            lwc.pushToStack(String.valueOf(val));
+          } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+          }
+        }).start();
+      }
+    }).start();
+    new Thread(() -> {
+      for (int i = 1; i < 6; i++) {
+        int val = i;
+        new Thread(() ->
+        {
+          try {
+            System.out.println("Received: " + lwc.popFromStack());
+          } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+          }
+        }).start();
+      }
+    }).start();
+  }
+
+  public void pushToStack(String item) throws Exception{
+    try {
+      lock.lock();
+      while(stack.size() == CAPACITY) {
+        stackFullCondition.await();
+      }
+      stack.push(item);
+      System.out.println("pushed: " + item);
+      stackEmptyCondition.signalAll();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public String popFromStack() throws Exception {
+    try {
+      lock.lock();
+      while(stack.size() == 0) {
+        stackEmptyCondition.await();
+      }
+      String popped = stack.pop();
+      System.out.println("popped: " + popped);
+      return popped;
+    } finally {
+      stackFullCondition.signalAll();
+      lock.unlock();
+    }
+  }
+
+}
