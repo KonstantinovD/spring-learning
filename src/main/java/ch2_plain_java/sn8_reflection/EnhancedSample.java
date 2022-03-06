97d3b4b9ba312bd40590b43ab9ebdd82ab4f5d80
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2022-03-06 15:59:02 +0300

    [JC] reflection, refl + parent/child classes

---
+++ src/main/java/ch2_plain_java/sn8_reflection/EnhancedSample.java
@@ -0,0 +1,11 @@
+package ch2_plain_java.sn8_reflection;
+
+public class EnhancedSample extends SampleClass {
+
+  private int i;
+
+  public EnhancedSample(String s, double d, int i) {
+    super(s, d);
+    this.i = i;
+  }
+}
---
+++ src/main/java/ch2_plain_java/sn8_reflection/SampleClass.java
@@ -0,0 +1,13 @@
+package ch2_plain_java.sn8_reflection;
+
+public class SampleClass {
+
+  private String s;
+  private double d;
+
+  public SampleClass(String s, double d) {
+    this.s = s;
+    this.d = d;
+  }
+
+}
