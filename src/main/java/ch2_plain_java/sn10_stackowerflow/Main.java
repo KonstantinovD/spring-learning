31dcf9639c40809513c44b4c0a9e40c83ec1394d
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2022-03-07 15:03:43 +0300

    [JC] empty method stack overflow

---
+++ src/main/java/ch2_plain_java/sn10_stackowerflow/Main.java
@@ -0,0 +1,13 @@
+package ch2_plain_java.sn10_stackowerflow;
+
+public class Main {
+
+  public static void empty() {
+    empty();
+  }
+
+  public static void main(String[] args) {
+    Main.empty();
+  }
+
+}
