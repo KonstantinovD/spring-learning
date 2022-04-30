2a21527fb741daac751550d8e5ce1b7a5abd9fc7
KonstantinovD <konstantinov_daniil407@mail.ru>
2022-04-30 13:34:56 +0300

    Bean constructors confusion + Check props by Environment class

--- src/main/java/book/p112_constructor_confusion/ConstructorConfusion.java
+++ src/main/java/book/p112_constructor_confusion/ConstructorConfusion.java
@@ -6,18 +6,44 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+
+// Иногда у Spring нет возможности выяснить, какой именно конструктор требуется
+// использовать для внедрения зависимостей. Как правило, это происходит в том случае,
+// если имеются два конструктора с одним и тем же количеством аргументов и
+// одинаково обозначенными их типами.
+// Например, у нас тут два конструктора: string/int параметры. Если ни один из них не
+// делать дефолтным, то получим ошибку:
+//
+// Failed to instantiate [book.p112_constructor_confusion.ConstructorConfusion]:
+//   No default constructor found;
+//
+// Поэтому на один из них надо повесить аннотацию @Autowired
 @Service("constructorConfusion")
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "book.p112_constructor_confusion")
 public class ConstructorConfusion {
   private String someValue;
 
-  public ConstructorConfusion(String someValue) {
+  // присвоение значения primary-полю бина
+  // @Value use guide: https://www.baeldung.com/spring-value-annotation
+  public ConstructorConfusion(@Value("${constructor_confusion_112.value}") String someValue) {
     System.out.println("ConstructorConfusion(String) called");
     this.someValue = someValue;
   }
+
+  @Autowired
+  private Environment env;
+
+  public String getProp() {
+    return env.containsProperty("constructor_confusion_112.kvak") + " " +
+        env.containsProperty("constructor_confusion_112.value");
+  }
+
+//  @Value("${constructor_confusion_112.kvak}")
+//  private boolean integrated;
 
   // присвоение значения primary-полю бина
   // @Value use guide: https://www.baeldung.com/spring-value-annotation
@@ -50,5 +76,7 @@ public class ConstructorConfusion {
     System.out.println("[BEAN]: " + beanConfusion);
     ConstructorConfusion manualConfusion = new ConstructorConfusion("programmatically created class");
     System.out.println("[MANUAL]: " + manualConfusion);
+
+    System.out.println(beanConfusion.getProp());
   }
 }
