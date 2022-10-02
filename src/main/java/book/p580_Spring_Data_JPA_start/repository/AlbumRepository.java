78b6346b4bc7ddb1f80faf65517b6c23e16c5d4e
KonstantinovD <konstantinov_daniil407@mail.ru>
2022-10-02 18:27:15 +0300

    Spring Data intro, CrudRepository, config Spring Data, @query custom requests [2]

--- src/main/java/book/p580_Spring_Data_JPA_start/repository/AlbumRepository.java
+++ src/main/java/book/p580_Spring_Data_JPA_start/repository/AlbumRepository.java
@@ -5,9 +5,11 @@ import book.p580_Spring_Data_JPA_start.entity.Singer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
   List<Album> findBySinger(Singer singer);
 
--- src/main/java/book/p580_Spring_Data_JPA_start/repository/SingerRepository.java
+++ src/main/java/book/p580_Spring_Data_JPA_start/repository/SingerRepository.java
@@ -2,9 +2,11 @@ package book.p580_Spring_Data_JPA_start.repository;
 
 import book.p580_Spring_Data_JPA_start.entity.Singer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SingerRepository
     extends CrudRepository<Singer, Integer> {
   List<Singer> findByFirstName(String firstName);
