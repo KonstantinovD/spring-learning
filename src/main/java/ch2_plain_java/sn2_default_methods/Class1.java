e0ce12f5efb4dd7814d2bc5b998ed9850ca5c69c
KonstantinovD <konstantinov_daniil407@mail.ru>
2022-11-15 23:06:36 +0300

    [JC|violet] Interfaces inheritance & methods conflicts [2]

--- src/main/java/ch2_plain_java/sn2_default_methods/Class1.java
+++ src/main/java/ch2_plain_java/sn2_default_methods/Class1.java
@@ -4,15 +4,16 @@ public abstract class Class1 implements Interface1, Interface2 {
 
   // method1 non-default в обоих интерфейсах, переопределять не надо
 
-  // конфликт, т к метод default, надо переопределить или объявить
-  // абстрактным
+  // конфликт, т к метод default хотя бы в одном из интерфейсов, надо переопределить
+  // или объявить абстрактным
   public abstract String method2();
-
 //  @Override
 //  public String method2() {
 //    return "Class1.method2() + " + Interface1.super.method2();
 //  }
 
+  // то же самое касается method3() - оба метода дефолтные - надо переопределить
+  // или объявлять абстрактным
   @Override
   public String method3() {
     return "Class1.method3() + " + Interface2.super.method3() +
