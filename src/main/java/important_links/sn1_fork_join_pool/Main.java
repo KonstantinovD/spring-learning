ffd4e4e0b91b564b1fd72c9c25b95cbf4c4b3d03
KonstantinovD <konstantinov_daniil407@mail.ru>
2022-11-20 09:29:50 +0300

    [JC] ForkJoinPool - RecursiveTask

---
+++ src/main/java/important_links/sn1_fork_join_pool/Main.java
@@ -0,0 +1,7 @@
+package important_links.sn1_fork_join_pool;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(ForkJoinAdd.startForkJoinSum(1_000_000)); // 500000500000
+  }
+}
