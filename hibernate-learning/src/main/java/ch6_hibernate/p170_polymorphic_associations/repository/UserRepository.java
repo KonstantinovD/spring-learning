977518b3be2e87719ca97416c4392d98a2e7576f
KonstantinovD <konstantinov_daniil407@mail.ru>
2023-12-25 16:36:43 +0300

    добавить аннотацию

--- hibernate-learning/src/main/java/ch6_hibernate/p170_polymorphic_associations/repository/UserRepository.java
+++ hibernate-learning/src/main/java/ch6_hibernate/p170_polymorphic_associations/repository/UserRepository.java
@@ -2,8 +2,10 @@ package ch6_hibernate.p170_polymorphic_associations.repository;
 
 import ch6_hibernate.p170_polymorphic_associations.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
