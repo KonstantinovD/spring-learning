b3ce948e37e62739a58831a11c422a005502b171
KonstantinovD <konstantinov_daniil407@mail.ru>
2023-12-30 23:39:52 +0300

    common TransactionalProcessor

---
+++ hibernate-learning/src/main/java/ch6_hibernate/common/TransactionalProcessor.java
@@ -0,0 +1,23 @@
+package ch6_hibernate.common;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.Callable;
+
+
+@Service
+public class TransactionalProcessor {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public <V> V executeInNewTransaction(Callable<V> action) throws Exception {
+        return action.call();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void runInNewTransaction(Runnable action) {
+        action.run();
+    }
+
+}
